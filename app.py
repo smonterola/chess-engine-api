4fbf19cb9c76a8bea8c1219824f6d207779b0ffa
Sam Monterola <sam@monterola.com>
2024-08-19 14:56:33 -0500

    updating app

--- app.py
+++ app.py
@@ -1,7 +1,27 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Body, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
 from fastapi.middleware.cors import CORSMiddleware
 
+from slowapi import Limiter
+from slowapi.util import get_remote_address
+from slowapi import _rate_limit_exceeded_handler
+from slowapi.errors import RateLimitExceeded
+import APIKEY
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+def api_key_auth(api_key: str = Depends(oauth2_scheme)):
+    if api_key not in APIKEY.APIKEYS:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Forbidden"
+        )
+
+limiter = Limiter(key_func=get_remote_address)
+
 app = FastAPI()
+app.state.limiter = limiter
+
+app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 
 origins = [
     "http://chess.roastlemon.com",
@@ -18,11 +38,14 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-@app.get("/")
-def home():
-    return {"Data": "Test"}
+@app.get("/", dependencies=[Depends(api_key_auth)])
+@limiter.limit("1/second")
+def home(
+    request: Request
+):
+    return {"Client host": request.client.host, "params": request.query_params}
 
-@app.get("/about")
+@app.get("/about", dependencies=[Depends(api_key_auth)])
 def about():
     return {"Data": "About"}
 
--- test_servers.py
+++ test_servers.py
@@ -1,5 +1,16 @@
 import requests
 
-print(requests.get("http://chess-api.roastlemon.com/about").json())
+headers = {
+    'Authorization': 'Bearer cd6b569e-42cd-4f2f-9132-f6b3f0d61d40'
+}
+print(requests.get(
+    url="http://chess-api.roastlemon.com", 
+    headers=headers, 
+    params={"fen":"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"}
+    ).json())
 
-print(requests.get("http://127.0.0.1:8000").json())
+print(requests.get(
+    url="http://127.0.0.1:8000", 
+    headers=headers, 
+    params={"fen":"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"}
+    ).json())
